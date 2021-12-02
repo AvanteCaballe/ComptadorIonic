67f62dc092caf20cc35f2c1d2ac8b54b1515ba50
AvanteCaballe <marcavante@iesebre.com>
2021-12-02 17:29:32 +0100

    Github Pages

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git remote add origin git@github.com:AvanteCaballe/ComptadorIonic.git || true
+
+git branch -M main || true
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:AvanteCaballe/ComptadorIonic.git main
+
+cd -
